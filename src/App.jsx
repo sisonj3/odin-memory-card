bddfda2db347edfada22ed3d8eb9907e7473919b
sisonj <98570174+sisonj3@users.noreply.github.com>
2024-02-24 13:14:47 -0800

    Added score functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  let addPoint = () => {
+    let newScore = score + 1;
+
+    setScore(newScore);
+
+    if (newScore > bestScore) {
+      setBestScore(newScore);
+    }
+  }
+
+  let resetScore = () => {
+    setScore(0);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <div>
+          <h1>Memory Game</h1>
+          <span>Get points by clicking on an image but don't click on any more than once!</span>
+        </div>
+        
+        <div>
+          <span>Score: {score}</span>
+          <span>Best Score: {bestScore}</span>
+        </div>
+      </header>
+
+      <main>
+        <button onClick={addPoint}>Add Point</button>
+        <button onClick={resetScore}>Reset</button>
+      </main>
     </>
   )
 }
